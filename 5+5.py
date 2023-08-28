0b1abee87b3d21b296142213bfb1602f478fabac
adesam <adesam654@gmail.com>
2023-08-28 15:42:03 -0700

    bike analysis upload

---
+++ 5+5.py
@@ -0,0 +1,6 @@
+import pandas as pd
+5+5
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+
---
+++ olypic.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+data=pd.read_csv(r'C:\Users\GREAT\Desktop\USA_covid.csv')
+data
+
