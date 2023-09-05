5809d682ab4013f5e440f0e7181e6b4d702f1b3e
adesam <adesam654@gmail.com>
2023-09-05 07:47:14 -0700

    covid analysis finanlly added

--- 5+5.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-5+5
-import numpy as np
-import seaborn as sns
-from matplotlib import pyplot as plt
-
--- olypic.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-from matplotlib import pyplot as plt
-import seaborn as sns
-
-data=pd.read_csv(r'C:\Users\GREAT\Desktop\USA_covid.csv')
-data
-
